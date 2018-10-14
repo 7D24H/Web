e7e578487cd45646dc08a406bc3fd58e600ed8e8
7D24H <31737338+7D24H@users.noreply.github.com>
2018-10-14 17:01:58 +0800

    cropper“

--- WebAssign1/scripts/singlePic.js
+++ WebAssign1/scripts/singlePic.js
@@ -20,6 +20,34 @@ function imgPreview(fileDom){
         var img = document.getElementById("preview");
         //图片路径设置为读取的图片
         img.src = e.target.result;
+        widthLonger(img,0);
     };
     reader.readAsDataURL(file);
 }
+
+function widthLonger(imgDom,angle){
+    if(angle==90||angle==270){
+        if(imgDom.width>imgDom.height){
+            imgDom.setAttribute("width","530px");
+        }else{
+            img.setAttribute("height","730px");
+        }
+    }else{
+        if(imgDom.width>imgDom.height){
+            imgDom.setAttribute("width","730px");
+        }else{
+            img.setAttribute("height","530px");
+        }
+    }
+
+}
+
+window.onload = function(){
+    var current = 0;
+    var img=document.getElementById("preview");
+    document.getElementById('rotateBtn').onclick = function(){
+        current = (current+90)%360;
+        img.style.transform = 'rotate('+current+'deg)';
+        widthLonger(img,current);
+    }
+};
