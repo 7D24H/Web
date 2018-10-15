58594fcefa9f233873ce29dc763583ebf8834f99
7D24H <31737338+7D24H@users.noreply.github.com>
2018-10-15 23:19:34 +0800

    edit finish DONT CHANGE ANYMORE

--- WebAssign1/scripts/singlePic.js
+++ WebAssign1/scripts/singlePic.js
@@ -35,8 +35,10 @@ function imgPreview(fileDom){
 
     };
     reader.readAsDataURL(file);
-    document.getElementById("edit").style.visibility="visible";
+    document.getElementById("edit").style.display="inline-block";
+    console.log(document.getElementById("edit").style);
     document.getElementById("inputFile").style.display="none";
+
 }
 
 
@@ -74,34 +76,82 @@ function imgPreview(fileDom){
 //         result.setAttribute("src",image.src);
 //     });
 // });
+var showPic=document.getElementsByClassName("showPic")[0];
+var border=document.getElementById("border");
+var small=document.getElementsByClassName("small")[0];
 
+var btnBar=document.getElementById("btnBar");
 var edit=document.getElementById("edit");
-var rotate=document.getElementById("rotate");
-edit.addEventListener("click",function(){
-    rotate.style.visibility="visible";
-    edit.style.visibility="hidden";
+var rotateRight=document.getElementById("rotateRight");
+var rotateLeft=document.getElementById("rotateLeft");
+var oneToOne=document.getElementById("oneToOne");
+var twoToThree=document.getElementById("twoToThree");
+var threeToFour=document.getElementById("threeToFour");
+var nineToSixteen=document.getElementById("nineToSixteen");
+var free=document.getElementById("free");
 
-    var image=document.getElementById("image");
+var image=document.getElementById("image");
+
+edit.addEventListener("click",function(){
+    border.style.marginLeft="45px";//图片往左撤
+
+    showPic.style.width="90%";//为了响应窗口大小变化
+    image.style.width="70%";
+    small.style.width="30%";
+    edit.style.display="none";
+
+    btnBar.style.display="inline-block";
+    rotateRight.style.backgroundImage="url(\"../assets/arrow-rotate-right.png\")"//不知道为什么重显以后加载不出背景图 再手动加一次
+    rotateLeft.style.backgroundImage="url(\"../assets/arrow-rotate-left.png\")";
+    //
+    // horizonTurn.style.backgroundImage="url(\"../assets/horizonTurn.png\")";
+    // verticalTurn.style.backgroundImage="url(\"../assets/verticalTurn.png\")";
+
+
     var cropper = new Cropper(image, {
         // aspectRatio: 16 / 9,
         viewMode:2,
         preview:".small",
         guides:false,
         crop: function (e) {
-            console.log(e.detail.x);
-            console.log(e.detail.y);
-            console.log(e.detail.width);
-            console.log(e.detail.height);
-            console.log(e.detail.rotate);
-            console.log(e.detail.scaleX);
-            console.log(e.detail.scaleY);
+            // console.log(e.detail.x);
+            // console.log(e.detail.y);
+            // console.log(e.detail.width);
+            // console.log(e.detail.height);
+            // console.log(e.detail.rotate);
+            // console.log(e.detail.scaleX);
+            // console.log(e.detail.scaleY);
         }
     });
 
-    rotate.addEventListener("click",function (ev) {
+    rotateRight.addEventListener("click",function (ev) {
         cropper.rotate(90);
     })
 
+    rotateLeft.addEventListener("click",function(ev){
+        cropper.rotate(-90);
+    })
+
+    oneToOne.addEventListener("click",function(ev){
+        cropper.setAspectRatio(1/1);
+    })
+
+    twoToThree.addEventListener("click",function(ev){
+        cropper.setAspectRatio(2/3);
+    })
+
+    threeToFour.addEventListener("click",function(ev){
+        cropper.setAspectRatio(3/4);
+    })
+
+    nineToSixteen.addEventListener("click",function(ev){
+        cropper.setAspectRatio(9/16);
+    })
+
+    free.addEventListener("click",function (ev) {
+        cropper.setAspectRatio(NaN);
+
+    })
 
 
 
